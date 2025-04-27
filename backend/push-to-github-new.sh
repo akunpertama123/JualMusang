15a9687b83f748d1004ec15c2fa3985f4d2039a1
akunpertama123 <94282654+akunpertama123@users.noreply.github.com>
2025-04-27 14:34:37 +0700

    Add backend/push-to-github-new.sh 1745739276072

---
+++ backend/push-to-github-new.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git add .
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/akunpertama123/template-marketplace-backend.git
+git push -u origin main
