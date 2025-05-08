87313bee25aa73fdbce53c203b5c670555b5ea35
akunpertama123 <94282654+akunpertama123@users.noreply.github.com>
2025-05-09 01:50:29 +0700

    Add marketplace/push-frontend-to-github.sh 1746730228234

---
+++ marketplace/push-frontend-to-github.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git add .
+git commit -m "Initial commit frontend"
+git branch -M main
+git remote add origin git@github.com:akunpertama123/JualMusang2.git
+git push -u origin main
